5693059228824e8749e8cdaf70b04c4277d5b282
ChrisMoskala <moskalakrzysztof01@gmail.com>
2024-05-21 16:44:34 +0200

    lawencja i skrypt

---
+++ Assets/Scripts/StaticVariables.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticVariables : MonoBehaviour
+{
+    public static string ciapagi;
+    public static string dworce;
+    public static string wagony;
+}
